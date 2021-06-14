14b6058c0146a4cf2f7449d2ff8bf06e1649dc24
David Bowland <dbowland1@gmail.com>
2021-06-13 22:17:03 -0500

    Add miscellaneous fixes

--- src/services/jokes.test.ts
+++ src/services/jokes.test.ts
@@ -5,17 +5,28 @@ import { rest, server } from '@test/setup-server'
 const baseUrl = process.env.JOKE_API_BASE_URL || 'http://localhost'
 
 describe('Joke service', () => {
-  const randomJokeResult = { '3': { joke: 'Ha' }, '74': { joke: 'Ha!' } }
+  const randomJokeResult = { '3': { joke: 'rofl' }, '74': { joke: 'lol' } }
 
-  beforeAll(() => {
+  beforeEach(() => {
     server.use(
       rest.get(`${baseUrl}/v1/jokes/random`, async (req, res, ctx) => {
+        if (JokeService.recentIndexes.join(',') !== req.url.searchParams.get('avoid')) {
+          return res(ctx.status(400))
+        }
         return res(ctx.json(randomJokeResult))
       })
     )
   })
 
   test('getRandomJokes returns results from client endpoint', async () => {
+    const result = await JokeService.getRandomJokes()
+
+    expect(JokeService.recentIndexes).toEqual(Object.keys(randomJokeResult))
+    expect(result).toEqual(Object.values(randomJokeResult))
+  })
+
+  test('getRandomJokes returns results using recentIndexes', async () => {
+    JokeService.recentIndexes = ['32', '45', '79']
     const result = await JokeService.getRandomJokes()
 
     expect(JokeService.recentIndexes).toEqual(Object.keys(randomJokeResult))
