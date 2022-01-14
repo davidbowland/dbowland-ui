bfb49c3eaf810164ec7508ed90267997584cb718
David Bowland <dbowland1@gmail.com>
2022-01-14 16:57:56 -0600

    Query for hosted zone ID

--- infrastructure/route53/index.ts
+++ infrastructure/route53/index.ts
@@ -2,3 +2,4 @@ import './records' // Why is this import necessary?!?
 // Changes aren't picked up without it, but it isn't necessary anywhere else.
 
 export * from './records'
+export * from './zones'
--- infrastructure/vars.ts
+++ infrastructure/vars.ts
@@ -1,15 +1,16 @@
 /* General */
 
+export const awsAccountId = process.env.AWS_ACCOUNT_ID
 export const createdBy = 'dbowland-ui'
 export const createdFor = 'dbowland.com'
 
 /* CDN */
 
-export const acmCertificateArn = 'arn:aws:acm:us-east-1:494887012091:certificate/6a48cba7-feb9-4de5-8cbf-d383140fcdef'
+export const acmCertificateArn = `arn:aws:acm:us-east-1:${awsAccountId}:certificate/6a48cba7-feb9-4de5-8cbf-d383140fcdef`
 export const redirectStaticDomain = 'dbowland-ui-redirect.s3-website.us-east-2.amazonaws.com'
 export const redirectToDomain = 'bowland.link'
 export const sourceS3Domain = 'dbowland-ui-source.s3.us-east-2.amazonaws.com'
 
 /* Route 53 */
 
-export const hostedZoneId = 'Z01312547RGU1BYKIJXY'
+export const domainName = 'bowland.link'
