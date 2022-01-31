45a4ab99e772a332bad115b49d49b343a5d1266f
David Bowland <dbowland1@gmail.com>
2022-01-30 21:04:33 -0600

    Use correct environment files on deploy

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -3,8 +3,6 @@ declare global {
     interface ProcessEnv {
       COGNITO_APP_CLIENT_ID: string
       COGNITO_USER_POOL_ID: string
-      JOKE_API_BASE_URL: string
-      JOKE_API_FETCH_COUNT: number
     }
   }
 }
