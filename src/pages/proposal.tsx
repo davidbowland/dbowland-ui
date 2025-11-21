76a9e986a14f1aadc3491431a720f9194597f4aa
David Bowland <dbowland1@gmail.com>
2025-11-20 23:59:43 -0600

    Change proposal video

--- src/pages/proposal.tsx
+++ src/pages/proposal.tsx
@@ -1,7 +1,7 @@
 import Redirect, { DriveFileRedirect, RedirectHead } from '@components/redirect'
 import React from 'react'
 
-const VIDEO_ID = '1_H9BnVXBdltPGgtgi_sUf-QZVvaFq4h1'
+const VIDEO_ID = '1QjF6bZjeD2k4OKReS_Okm7xX0W_l9n52'
 
 const Proposal = (): JSX.Element => {
   return <Redirect id={VIDEO_ID} type={DriveFileRedirect} />
