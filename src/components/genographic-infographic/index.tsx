f900850dee09b627cbbbc295e551cf0a0e339667
David Bowland <dbowland1@gmail.com>
2026-03-25 23:50:07 -0500

    feat: migrate genographic-infographic to next/link and next-export-optimize-images

--- src/components/genographic-infographic/index.tsx
+++ src/components/genographic-infographic/index.tsx
@@ -1,10 +1,11 @@
-import { Link } from 'gatsby'
-import { StaticImage } from 'gatsby-plugin-image'
+import Image from 'next-export-optimize-images/image'
+import Link from 'next/link'
 import React from 'react'
 
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 
+import genographicInfographic from '@assets/images/genographic-infographic.png'
 import genographicPdf from '@assets/pdf/genographic-results.pdf'
 
 const GenographicInfographic = (): JSX.Element => {
@@ -16,10 +17,10 @@ const GenographicInfographic = (): JSX.Element => {
         participation in that project. People who know me will not be surprised to learn that my ancestors were
         &quot;[h]unters of megafauna such as the woolly mammoth.&quot; &#x1F606;
       </Typography>
-      <Link to={genographicPdf}>
-        <StaticImage
+      <Link href={genographicPdf}>
+        <Image
           alt="41% Eastern Europe; 22% Western and Central Europe; 20% Scandinavin; 14% Great Britain and Ireland; 2% Asia Minor; 1.1% Neanderthal"
-          src="../../assets/images/genographic-infographic.png"
+          src={genographicInfographic}
         />
       </Link>
       <Typography sx={{ fontSize: 'smaller', fontStyle: 'italic' }}>
