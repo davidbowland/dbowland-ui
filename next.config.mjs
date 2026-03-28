df7d6e3b1ff11b4ae87cb18632fadfc2d2f97bcb
David Bowland <dbowland1@gmail.com>
2026-03-28 10:21:43 -0500

    Export images and allow trailing slashes

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 /** @type {import('next').NextConfig} */
 const nextConfig = withExportImages({
   output: 'export',
+  trailingSlash: true,
   compiler: {
     styledComponents: true,
   },
